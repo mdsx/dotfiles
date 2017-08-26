fdbe4ca0359dce4dd8f3c1f15322145f95cb0889
Mihir Sherlekar <mhrsherlekar@gmail.com>
2017-08-26 16:23:20 -0700

    bar now controlls both mopidy/ncmpcpp and spotify gui; added status tray bar hidden with hideIt.sh

--- .config/polybar/scripts/playpause.py
+++ .config/polybar/scripts/playpause.py
@@ -1,6 +1,9 @@
 #!usr/bin/python
 
-import os
+import os, sys
+
+sys.tracebacklimit = None
+
 cmd = os.popen('playerctl -p spotify status').read()
 cmd = cmd.split('\n')
 cmd = cmd[0]
@@ -10,5 +13,15 @@ if cmd == "Playing":
 elif cmd == 'Paused':
      print('')
 else:
-    print('')
-    # print('')
+    try:
+        cmd = os.popen('mpc status 2>/dev/null').read()
+        cmd = cmd.split('\n')
+        cmd = cmd[1].split(' ')[0]
+        if cmd == '[playing]':
+            print('')
+        elif cmd == '[paused]':
+            print('')
+        else:
+            print('')
+    except Exception:
+        print('')
--- .config/polybar/scripts/spoti.py
+++ .config/polybar/scripts/spoti.py
@@ -6,10 +6,16 @@ cmd = os.popen('playerctl -p spotify status').read()
 cmd = cmd.split('\n')
 cmd = cmd[0]
 
-meta_artist = os.popen('playerctl -p spotify metadata xesam:artist').read()
-meta_title = os.popen('playerctl -p spotify metadata xesam:title').read()
+if cmd == 'Not available':
+    cmd = os.popen('mpc status').read()
+    cmd = cmd.split('\n')
+    cmd = cmd[0]
+    print(cmd)
+else:
+    meta_artist = os.popen('playerctl -p spotify metadata xesam:artist').read()
+    meta_title = os.popen('playerctl -p spotify metadata xesam:title').read()
 
-if cmd == "Playing":
-    print(meta_title + ' - ' + meta_artist)
-elif cmd == 'Paused':
-    print(meta_title + ' - ' + meta_artist)
+    if cmd == "Playing":
+        print(meta_title + ' - ' + meta_artist)
+    elif cmd == 'Paused':
+        print(meta_title + ' - ' + meta_artist)
